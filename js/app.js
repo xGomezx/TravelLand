bf4414e2795032bfcd6ef4f006f5f94db5bd1280
Juan José Gómez <112130665+xGomezx@users.noreply.github.com>
2024-03-06 17:31:47 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const check = document.getElementById('check')
+
+
+check.addEventListener('click',idioma)
+
+function idioma() {
+    let id = check.checked;
+
+    if (id==true) {
+        location.href='es/index.html'
+    } else {
+        location.href='../index.html'
+    }
+}
+
+    
